b4951f625fd884ff58bea316f08693a85ee8567d
Mariah <mariah.zammit.18@um.edu.mt>
2021-02-24 12:47:10 +0100

    Added Bootstrap for Data. Closes #14

--- pet-clinic-data/src/main/java/com/example/sfgpetclinic/services/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/com/example/sfgpetclinic/services/map/OwnerServiceMap.java
@@ -1,11 +1,11 @@
 package com.example.sfgpetclinic.services.map;
 
 import com.example.sfgpetclinic.model.Owner;
-import com.example.sfgpetclinic.services.CrudService;
+import com.example.sfgpetclinic.services.OwnerService;
 
 import java.util.Set;
 
-public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long> {
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements OwnerService {
 
     @Override
     public Set<Owner> findAll() {
@@ -30,5 +30,10 @@ public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements 
     @Override
     public Owner save(Owner object) {
         return super.save(object.getId(), object);
+    }
+
+    @Override
+    public Owner findByLastName(String lastName) {
+        return null;
     }
 }
