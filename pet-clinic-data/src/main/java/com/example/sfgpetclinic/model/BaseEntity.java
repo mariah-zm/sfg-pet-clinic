6cc033525350ddf8fbd8b40a3a880d5db643c998
Mariah <mariah.zammit.18@um.edu.mt>
2021-03-02 11:34:18 +0100

    Implemented Base Entity. Closes #36

--- pet-clinic-data/src/main/java/com/example/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/example/sfgpetclinic/model/BaseEntity.java
@@ -1,9 +1,16 @@
 package com.example.sfgpetclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
@@ -13,6 +20,5 @@ public class BaseEntity implements Serializable {
     public void setId(Long id) {
         this.id = id;
     }
-
 
 }
