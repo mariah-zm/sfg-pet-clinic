1e9b687f70cf268a5768f34dc9678bd8331a385c
Mariah <mariah.zammit.18@um.edu.mt>
2021-03-02 12:13:45 +0100

    Created Spring Data JPA Service for Owners. Closes #41

--- pet-clinic-data/src/main/java/com/example/sfgpetclinic/repositories/VetRepository.java
+++ pet-clinic-data/src/main/java/com/example/sfgpetclinic/repositories/VetRepository.java
@@ -1,6 +1,7 @@
 package com.example.sfgpetclinic.repositories;
 
+import com.example.sfgpetclinic.model.Vet;
 import org.springframework.data.repository.CrudRepository;
 
-public interface VetRepository extends CrudRepository<VetRepository, Long> {
+public interface VetRepository extends CrudRepository<Vet, Long> {
 }
