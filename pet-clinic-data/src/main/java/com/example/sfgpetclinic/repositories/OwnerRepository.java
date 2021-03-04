dbfd6817cc5e665db1d1690969e39cfcf64af2a4
Mariah <mariah.zammit.18@um.edu.mt>
2021-03-04 14:04:15 +0100

    Implemented Find Owner. Closes #56

--- pet-clinic-data/src/main/java/com/example/sfgpetclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/com/example/sfgpetclinic/repositories/OwnerRepository.java
@@ -3,8 +3,12 @@ package com.example.sfgpetclinic.repositories;
 import com.example.sfgpetclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
 
     Owner findByLastName(String lastname);
+
+    List<Owner> findAllByLastNameLike(String lastName);
 
 }
